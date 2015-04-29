73c561872d404b341f5aa28458de46e0bcd82736
Corpelijn <bassiecorpelijn@hotmail.com>
2015-04-29 10:06:16 +0200

    Initial commit

---
+++ src/test/project/audiochat/VoiceClient.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.project.audiochat;
+
+/**
+ *
+ * @author Bas
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Trivial client for the date server.
+ */
+public class VoiceClient {
+
+    /**
+     * Runs the client as an application.  First it displays a dialog
+     * box asking for the IP address or hostname of a host running
+     * the date server, then connects to it and displays the date that
+     * it serves.
+     */
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        String serverAddress = /*"127.0.0.1";*/JOptionPane.showInputDialog(
+            "Enter IP Address of a machine that is\n" +
+            "running the date service on port 9090:");
+        Socket s = new Socket(serverAddress, 9090);
+        ObjectInputStream is = new ObjectInputStream(s.getInputStream());
+        //BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        Date d = (Date)is.readObject();
+        //String answer = input.readLine();
+        String answer = d.toString();
+        JOptionPane.showMessageDialog(null, answer);
+        System.exit(0);
+    }
+}
---
+++ src/test/project/audiochat/VoiceServer.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.project.audiochat;
+
+
+/**
+ *
+ * @author Bas
+ */
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * A TCP server that runs on port 9090.  When a client connects, it
+ * sends the client the current date and time, then closes the
+ * connection with that client.  Arguably just about the simplest
+ * server you can write.
+ */
+public class VoiceServer {
+
+    /**
+     * Runs the server.
+     */
+    public static void main(String[] args) throws IOException {
+        ServerSocket listener = new ServerSocket(9090);
+        try {
+            while (true) {
+                System.out.println("Connecting...");
+                Socket socket = listener.accept();
+                try {
+                    ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+                    //PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    //out.println(new Date().toString());
+                    os.writeObject(new Date());
+                    System.out.println("Connected to: " + socket.getRemoteSocketAddress().toString());
+                } finally {
+                    socket.close();
+                }
+            }
+        }
+        finally {
+            listener.close();
+        }
+    }
+}
