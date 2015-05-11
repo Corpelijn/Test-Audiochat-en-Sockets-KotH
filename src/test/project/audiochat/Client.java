7a294e25d8b7f85324747a147aecd0fcb4fdb421
Corpelijn <bassiecorpelijn@hotmail.com>
2015-05-11 09:30:06 +0200

    new chat system for RMI

--- src/test/project/audiochat/Client.java
+++ src/test/project/audiochat/Client.java
@@ -5,79 +5,18 @@
  */
 package test.project.audiochat;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  *
  * @author Bas
  */
-public class Client extends Thread {
+import java.io.IOException;
 
-    private Socket socket;
-    private ObjectOutputStream sender;
-    private ObjectInputStream reader;
-    private List<Message> messages;
+/**
+ * Trivial client for the date server.
+ */
+public class Client {
 
-    public Socket getSocket() {
-        return this.socket;
+    public static void main(String[] args) throws IOException {
+        new VoiceServer().start();
     }
-
-    public void setSocket(Socket socket) {
-        this.socket = socket;
-    }
-
-    public Client(Socket socket, VoiceServer parent) {
-        this.socket = socket;
-        this.messages = new ArrayList<>();
-        try {
-            sender = new ObjectOutputStream(this.socket.getOutputStream());
-            reader = new ObjectInputStream(this.socket.getInputStream());
-        } catch (Exception ex) {
-        }
-        
-        MessageReader ms = new MessageReader(parent, reader);
-        ms.start();
-    }
-
-    public synchronized void addMessage(String message) {
-        if(message.equals("audio"))
-            messages.add(new Message("audio", message));
-        else
-            messages.add(new Message("string", message));
-    }
-
-    @Override
-    public void run() {
-        messages.add(new Message("string", "Welcome"));
-        try {
-            while (true) {
-                synchronized (messages) {
-                    if (messages.size() > 0) {
-                        try {
-                            sender.writeObject(messages.get(0));
-                            if (messages.get(0).equals("exit")) {
-                                break;
-                            }
-                            messages.remove(0);
-                        } catch (IOException ex) {
-                        }
-                    }
-                }
-
-            }
-        } finally {
-            try {
-                sender.close();
-                this.socket.close();
-            } catch (IOException ex) {
-            }
-
-        }
-    }
-
 }
--- src/test/project/audiochat/VoiceServer.java
+++ src/test/project/audiochat/VoiceServer.java
@@ -5,62 +5,12 @@
  */
 package test.project.audiochat;
 
-
 /**
  *
  * @author Bas
  */
-
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-
-/**
- * A TCP server that runs on port 9090.  When a client connects, it
- * sends the client the current date and time, then closes the
- * connection with that client.  Arguably just about the simplest
- * server you can write.
- */
 public class VoiceServer {
-
-    /**
-     * Runs the server.
-     */
-    private ArrayList<Client> clients = new ArrayList<>();
-    
-    public void start() throws IOException {
-        ServerSocket listener = new ServerSocket(9090);
-       
-        
-        try {
-            while (true) {
-                System.out.println("Connecting...");
-                Socket socket = listener.accept();
-
-                Client c = new Client(socket, this);
-                c.start();
-                clients.add(c);
-            }
-        }
-        finally {
-            listener.close();
-        }
-    }
-    
-    public void SendMessage(String message)
+    public void start()
     {
-        for(Client c : clients)
-        {
-            c.addMessage(message);
-        }
-    }
-    
-    public void Exit()
-    {
-        System.exit(0);
     }
 }
