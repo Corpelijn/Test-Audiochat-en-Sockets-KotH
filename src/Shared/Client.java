1ac8cea0830c780e1c6d019f8e9dac7402babea1
Corpelijn <bassiecorpelijn@hotmail.com>
2015-05-13 15:48:43 +0200

    Working chat kicking by host fixed

--- src/Shared/Client.java
+++ src/Shared/Client.java
@@ -13,8 +13,6 @@ import java.io.Serializable;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -28,6 +26,7 @@ public class Client implements Serializable {
     private List<Client> knownClients;
     private int clientID;
     private String name;
+    private boolean isAlive = true;
 
     private static int clientCounter = 0;
 
@@ -74,10 +73,16 @@ public class Client implements Serializable {
 
         if (message.getSender() == this.clientID) {
             message.setSenderName(this.name);
+            if (!this.isAlive) {
+                return;
+            }
         }
 
         for (Client c : knownClients) {
             if (message.getSender() == c.clientID) {
+                if (!c.isAlive) {
+                    return;
+                }
                 message.setSenderName(c.name);
                 break;
             }
@@ -86,6 +91,9 @@ public class Client implements Serializable {
         // The message now has a client name
         // Send the message
         try {
+            if (!this.isAlive) {
+                return;
+            }
             sender.writeObject(message);
         } catch (Exception ecx) {
             System.out.println(ecx.getMessage());
@@ -93,65 +101,69 @@ public class Client implements Serializable {
     }
 
     private void startMessageReader() {
-        Thread t = new Thread(new Runnable() {
+        Thread t = new Thread(() -> {
+            while (true) {
+                if (!isAlive) {
+                    break;
+                }
 
-            @Override
-            public void run() {
-                while (true) {
-                    Message object = null;
-                    try {
-                        object = (Message) reader.readObject();
-                    } catch (Exception ex) {
+                Message object = null;
+                try {
+                    object = (Message) reader.readObject();
+                } catch (Exception ex) {
+                    continue;
+                }
+
+                System.out.println("Message received from " + socket.getInetAddress());
+
+                if (object instanceof InfoMessage) {
+                    InfoMessage mess = (InfoMessage) object;
+                    if (mess.getDefine().equals("CLIENT_NAME")) {
+                        System.out.print("\r<< Client(" + clientID + ") changed name from \'" + name + "\' to \'" + mess.getData() + "\' >>\n");
+                        name = (String) mess.getData();
+                        continue;
+                    } else if (mess.getDefine().equals("KICK_CLIENT")) {
+                        System.out.print("\r<< Client " + mess.getData() + " will be kicked >>\n");
+                        Client c = getClient((int) mess.getData());
+                        c.sendMessage(new InfoMessage(null, "KICK"));
+                        c.setClientDead();
                         continue;
                     }
+                }
 
-                    System.out.println("Message received from " + socket.getInetAddress());
+                VoiceServer.lastMessages.add(object);
+                for (Client c : knownClients) {
+                    c.sendMessage(object);
+                }
+                sendMessage(object);
 
-                    if (object instanceof InfoMessage) {
-                        InfoMessage mess = (InfoMessage) object;
-                        if (mess.getDefine().equals("CLIENT_NAME")) {
-                            System.out.print("\r<< Client(" + clientID + ") changed name from \'" + name + "\' to \'" + mess.getData() + "\' >>\n>");
-                            name = (String) mess.getData();
-                            continue;
-                        }
-                        else if (mess.getDefine().equals("KICK_CLIENT")) {
-                            System.out.print("\r<< Client " + mess.getData() + " will be kicked >>\n>");
-                            Client c = getClient((int)mess.getData());
-                            c.sendMessage(new InfoMessage(null, "KICK"));
-                            break; 
-                        }
-                    }
-
-                    VoiceServer.lastMessages.add(object);
-                    for (Client c : knownClients) {
-                        c.sendMessage(object);
-                    }
-                    sendMessage(object);
-
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException ex) {
-                    }
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException ex) {
                 }
             }
-
         });
         t.start();
     }
-    
-    
-    private Client getClient(int id)
-    {
+
+    private void setClientDead() {
+        this.isAlive = false;
+    }
+
+    private Client getClient(int id) {
         if (id == this.clientID) {
             return this;
         }
 
         for (Client c : knownClients) {
+            if (!c.isAlive) {
+                continue;
+            }
             if (id == c.clientID) {
                 return c;
             }
         }
-        
+
         return null;
     }
 }
